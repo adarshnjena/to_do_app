392c3f8c94354ced839fbac94d1cb236b026f057
Adarsh Jena <76236137+adarshnjena@users.noreply.github.com>
2021-12-07 18:17:20 +0530

    commit

--- lib/widget/task_list.dart
+++ lib/widget/task_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:to_do/models/task_data.dart';
 import 'package:to_do/widget/task_tile.dart';
@@ -14,6 +15,28 @@ class TaskList extends StatelessWidget {
           itemBuilder: (context, index) {
             return GestureDetector(
               onLongPress: () {
+                final snackBar = SnackBar(
+                  backgroundColor: Colors.black,
+                  content: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Task ${taskData.tasks[index].name} Deleated !!',
+                        style: GoogleFonts.nunito(
+                          fontSize: 18,
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Icon(
+                        Icons.delete,
+                        color: Colors.lightBlueAccent,
+                      ),
+                    ],
+                  ),
+                );
+                ScaffoldMessenger.of(context)
+                  ..hideCurrentSnackBar()
+                  ..showSnackBar(snackBar);
                 taskData.removeTask(index);
               },
               child: TaskTile(
